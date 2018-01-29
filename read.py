fc505e90aeaf7ef068d18e0a6fa8fc78ce9fe2ca
rdbadra <rdbadra@gmail.com>
2018-01-29 12:44:36 +0000

    check 6

--- read.py
+++ read.py
@@ -10,26 +10,4 @@ response = requests.post(os.environ['FARMWARE_URL'] + '/api/v1/bot/state',
 bot_state = response.json()
 
 position_x = response.json()['location_data']['position']['x']
-pin_13_value = response.json()['pins']['13']['value']
-
-send_message = {
-  "kind": "send_message",
-  "args": {
-    "message": "value: ",
-    "message_type": "success"
-  },
-  "body": [
-    {
-      "kind": "channel",
-      "args": {
-        "channel_name": "toast"
-      }
-    }
-  ]
-}
-headers = {
-  'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
-  'content-type': "application/json"}
-payload = json.dumps(send_message)
-requests.post(os.environ['FARMWARE_URL'] + '/api/v1/celery_script',
-              data=payload, headers=headers)
+#pin_13_value = response.json()['pins']['13']['value']
