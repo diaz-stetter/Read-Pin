ab3ea8a1550d176a3683c285df0a014b725c822e
rdbadra <rdbadra@gmail.com>
2018-01-29 12:40:05 +0000

    check 3

--- read.py
+++ read.py
@@ -2,10 +2,20 @@ import os
 import requests
 import json
 
+headers = {
+  'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
+  'content-type': "application/json"}
+response = requests.post(os.environ['FARMWARE_URL'] + '/api/v1/bot/state',
+              headers=headers)
+bot_state = response.json()
+
+position_x = response.json()['location_data']['position']['x']
+pin_13_value = response.json()['pins']['13']['value']
+
 send_message = {
   "kind": "send_message",
   "args": {
-    "message": "Bot is at position {{ x }}, {{ y }}, {{ z }}.",
+    "message": "value: " + str(pin_13_value),
     "message_type": "success"
   },
   "body": [
